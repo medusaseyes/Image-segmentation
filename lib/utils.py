562388865dfa5804627748926a2b408192982788
Gabrock <gabriel.gabrielroch@gmail.com>
2022-02-22 17:49:23 -0300

    Conversão e remoção de imagens .tif OK

--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import glob
+import glob, os
 
 def image_conversion(img_dir):
     for filename in glob.iglob(img_dir + '**/*.bmp', recursive=True):
@@ -12,25 +12,11 @@ def image_conversion(img_dir):
             print(f"{filename} = {image.mode}")
 
     for filename in glob.iglob(img_dir + '**/*.tif', recursive=True):
-        print(filename)
         image = Image.open(filename)
         filename = filename.replace(chr(92), "/").replace(img_dir+"/", "")
-        print(image.format)
         
         if image.format == "TIFF":
+            trash = f"{img_dir}/{filename}"
             filename = filename.replace(".tif", ".bmp")
             image.save(f"{img_dir}/{filename}")
-"""
-    for i in range(1,10):    
-        if i<10:
-            filename =  "C:/Users/rosid/medusas/Image-segmentation/data/0" +  str(i) +  ".tif"
-            img = Image.open(filename)
-            filename_bmp =  "C:/Users/rosid/medusas/Image-segmentation/data/0" +  str(i) +  ".bmp"
-            img.save(filename_bmp)
-        if i>9:
-            filename =  "C:/Users/rosid/medusas/Image-segmentation/data/" +  str(i) +  ".tif"
-            img = Image.open(filename)
-            filename_bmp =  "C:/Users/rosid/medusas/Image-segmentation/data/" +  str(i) +  ".bmp"
-            img.save(filename_bmp)
- """       
-image_conversion("../data/01/train")
+            os.remove(trash)
